9d24fa1abdee78c7ec6ab760b4a279d17d582d4f
Paola Covone <paoandrec21@gmail.com>
2023-07-07 16:43:36 -0500

    Se realizan cambios al código

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//DOM, cambio de cantidad artículos
-
+//DOM, cambio de cantidad de artículos
 let minusBtn = document.querySelector(".input__minus");
 let plusBtn = document.querySelector(".input__plus");
 let userInput = document.querySelector(".input__number");
@@ -21,7 +20,7 @@ minusBtn.addEventListener("click", () => {
   console.log(userInputNumber);
 });
 
-//Agregar el total de productos al carrito cuando se presiona el botón Añadir al carrito
+// Agregar el total de productos al carrito cuando se presiona el botón Añadir al carrito
 const addToCartBtn = document.querySelector(".cart__button");
 let cartNotification = document.querySelector(".header__cart--notification");
 let lastValue = parseInt(cartNotification.innerText);
@@ -35,24 +34,26 @@ addToCartBtn.addEventListener("click", () => {
   priceModal.innerHTML = `$4.99 x ${lastValue} <span>$${
     lastValue * 4.99
   }</span>`;
+
+  localStorage.setItem("cartValue", lastValue.toString());
 });
 
-//Mostrar el modal con el detalle del carrito
+// Mostrar el modal con el detalle
 const cartIconBtn = document.querySelector(".header__cart");
 const cartModal = document.querySelector(".cart-modal");
-// let priceModal = document.querySelector(".cart-modal__price");
 const productContainer = document.querySelector(
   ".cart-modal__checkout-container"
 );
+let priceModal = document.querySelector(".cart-modal__price");
 
 cartIconBtn.addEventListener("click", () => {
   cartModal.classList.toggle("show");
-  if (lastValue == 0) {
+  if (lastValue === 0) {
     drawProductInModal();
   }
 });
 
-//Borrar contenido del carrito
+// Borrar contenido del carrito
 function deleteProduct() {
   const deleteProductBtn = document.querySelector(".cart-modal__delete");
 
@@ -61,163 +62,44 @@ function deleteProduct() {
       '<p class="cart-empty">Tu carrito está vacío</p>';
     lastValue = 0;
     cartNotification.innerText = lastValue;
+    priceModal.innerHTML = "";
+    // Eliminar el valor del carrito del almacenamiento local
+    localStorage.removeItem("cartValue");
   });
 }
 
-//FUNCIONES
+// FUNCIONES
 function drawProductInModal() {
   productContainer.innerHTML = `<div class="cart-modal__details-container">
-  <img src="./recursos/piano.jpg" alt="piano" />
-  <div>
-    <p class="cart-modal__product">RockJam kit de piano...</p>
-    <p class="cart-modal__price">$4.99 x 3 <span>$14.97</span></p>
+    <img src="./recursos/piano.jpg" alt="piano" />
+    <div>
+      <p class="cart-modal__product">RockJam kit de piano...</p>
+      <p class="cart-modal__price">$4.99 x ${lastValue} <span>$${
+    lastValue * 4.99
+  }</span></p>
+    </div>
+    <div class="cart-modal__delete-img">
+      <img class="cart-modal__delete" src="./recursos/icon-delete.svg" alt="delete" />
+    </div>
   </div>
-  <div class="cart-modal__delete-img">
-    <img
-      class="cart-modal__delete"
-      src="./recursos/icon-delete.svg"
-      alt="delete"
-    />
-  </div>
-</div>
-<button class="cart-modal__checkout">Checkout</button>
-`;
+  <button class="cart-modal__checkout">Checkout</button>
+  `;
   deleteProduct();
-  let priceModal = document.querySelector(".cart-modal__price");
+  priceModal = document.querySelector(".cart-modal__price");
   priceModal.innerHTML = `$4.99 x ${lastValue} <span>$${
     lastValue * 4.99
   }</span>`;
 }
 
-// const cestaProductos = [];
-// const salir = 3;
-
-// class Producto {
-//   constructor(id, nombre, precio, categoria) {
-//     this.id = id;
-//     this.nombre = nombre;
-//     this.precio = precio;
-//     this.categoria = categoria;
-//   }
-// }
-
-// const arrayProductos = [
-//   new Producto(1, "Viola 16 pulgadas", 650, "instrumentos"),
-//   new Producto(2, "Violin 4/4", 500, "instrumentos"),
-//   new Producto(
-//     3,
-//     "RockJam Kit de piano de teclado con pantalla táctil",
-//     300,
-//     "instrumentos"
-//   ),
-//   new Producto(
-//     4,
-//     "D Z Strad - Viola de 15,5 pulgadas hecha a mano",
-//     1500,
-//     "instrumentos"
-//   ),
-//   new Producto(
-//     5,
-//     "Sabomenia Violin Dreamer D10 hecho a mano",
-//     299,
-//     "instrumentos"
-//   ),
-//   new Producto(
-//     6,
-//     "Starument Piano de teclado eléctrico prémium",
-//     149,
-//     "instrumentos"
-//   ),
-//   new Producto(7, "D'Addario Kaplan Premium Rosin", 8, "accesorios"),
-//   new Producto(
-//     8,
-//     "Bernardel Original – Colofonia para violín y viola",
-//     12,
-//     "accesorios"
-//   ),
-// ];
-
-// const mostrarPorCategoria = (categoria) => {
-//   const filtrado = arrayProductos.filter((el) => el.categoria === categoria);
-//   let mensajeAMostrar = filtrado
-//     .map(
-//       (el) => `ID: ${el.id} \nProducto: ${el.nombre} \nPrecio: ${el.precio} \n`
-//     )
-//     .join("");
-//   const id = parseInt(
-//     prompt(
-//       `${mensajeAMostrar}\nIngrese el ID del producto para agregar al carrito`
-//     )
-//   );
-//   const productoEncontrado = arrayProductos.find((el) => el.id === id);
-//   cestaProductos.push(productoEncontrado);
-// };
-
-// const verProducto = () => {
-//   let option;
-//   option = parseInt(
-//     prompt(
-//       "Elige la categoría que quieres ver: \n 1) Instrumentos \n 2) Accesorios \n 3) Ninguna"
-//     )
-//   );
-//   while (option !== salir) {
-//     switch (option) {
-//       case 1:
-//         mostrarPorCategoria("instrumentos");
-//         break;
-//       case 2:
-//         mostrarPorCategoria("accesorios");
-//         break;
-//       default:
-//         alert("Has ingresado una opción inválida.");
-//     }
-//     option = parseInt(
-//       prompt(
-//         "Elige la categoría que quieres ver: \n 1) Instrumentos \n 2) Accesorios \n 3) Ninguna"
-//       )
-//     );
-//   }
-// };
-
-// const verCarrito = () => {
-//   let mensajeAMostrar = "Lista de productos: \n";
-//   mensajeAMostrar += cestaProductos
-//     .map(
-//       (el) => `ID: ${el.id} \nProducto: ${el.nombre} \nPrecio: ${el.precio} \n`
-//     )
-//     .join("");
-//   const total = cestaProductos.reduce(
-//     (acumulador, elemento) => acumulador + elemento.precio,
-//     0
-//   );
-//   mensajeAMostrar += `El total de los productos añadidos es: ${total}`;
-//   alert(mensajeAMostrar);
-// };
-
-// let opcion = parseInt(
-//   prompt(
-//     "Elige la operación que deseas: \n 1) Ver Productos \n 2) Ver Carrito \n 3) Ninguna, salir"
-//   )
-// );
-
-// while (opcion !== salir) {
-//   switch (opcion) {
-//     case 1:
-//       verProducto();
-//       break;
-//     case 2:
-//       verCarrito();
-//       break;
-//     default:
-//       alert("Has ingresado una opción inválida.");
-//       break;
-//   }
-
-//   opcion = parseInt(
-//     prompt(
-//       "Elige la operación que deseas: \n 1) Ver Productos \n 2) Ver Carrito \n 3) Ninguna, salir"
-//     )
-//   );
-// }
-
-// alert("¡Gracias! ¡Vuelve pronto!");
+// Obtener el valor del carrito almacenado en el localStorage al cargar la página
+document.addEventListener("DOMContentLoaded", () => {
+  const storedValue = localStorage.getItem("cartValue");
+  if (storedValue) {
+    lastValue = parseInt(storedValue);
+    cartNotification.innerText = lastValue;
+    cartNotification.style.display = lastValue > 0 ? "block" : "none";
+    priceModal.innerHTML = `$4.99 x ${lastValue} <span>$${
+      lastValue * 4.99
+    }</span>`;
+  }
+});
